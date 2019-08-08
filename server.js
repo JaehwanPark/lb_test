cc586a44c113f58ec2262349efb24a27d05b8472
JaehwanPark <windy80x@gmail.com>
2019-08-08 15:23:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = http.createServer(function(request,response){
 
     response.writeHead(200,{'Content-Type':'text/html'});
     //response.end('Hello node.js!!');
-    response.end(os.networkInterfaces());
+    response.end(JSON.stringify(os.networkInterfaces(),null,2));
 
 });
 
